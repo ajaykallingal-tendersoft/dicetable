e485dbe23f73932a065c0f79f602a116f1ed6fbd
Ajay <ajay@tendersoftware.in>
2025-12-29 14:57:29 +0530

    login_screen_logo_fix

--- lib/src/ui/cafe_owner/authentication/login/login_screen.dart
+++ lib/src/ui/cafe_owner/authentication/login/login_screen.dart
@@ -176,12 +176,12 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                       Positioned(
                         top: 50,
-                        bottom: 650.h,
                         left: 0,
                         right: 0,
-                        child: Center(
+                        child: Align(
+                          alignment: Alignment.topCenter,
                           child: Container(
-                                height: 200.h,
+                                height: 200,
                                 width: 325,
                                 decoration: BoxDecoration(
                                   color: AppColors.primary,
